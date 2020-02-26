0f207e76657bc5b8613e5b74845cae95419bc82d
Nerio <McNerio>
2020-02-26 14:29:45 -0600

    Inicio de Reporteria

--- src/java/com/helpdesk/reportes/RequisicionPDF.java
+++ src/java/com/helpdesk/reportes/RequisicionPDF.java
@@ -54,7 +54,7 @@ public class RequisicionPDF extends HttpServlet {
 
             //Enlistar otros parametros
             Map params = new HashMap();
-            params.put("ID_Req", idRequisicion);
+            params.put("ID_Req", 3);
             params.put("SUBREPORT_DIR", pathSubSource);
 
             byte[] bytes = null;
